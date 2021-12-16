4454c3e07a8040dcab475d899c33f9c64dc8db79
glebmark <sechastwothree@protonmail.com>
2021-12-17 00:40:45 +0300

    added SVG

--- src/index.js
+++ src/index.js
@@ -124,10 +124,10 @@ function appendData(data) {
             tempContainer.id = "tempContainer" + this.dayNumber;
             this.tempContainer = tempContainer;
             tempContainer.classList.add("tempContainerClass");
-            tempContainer.style.border = "1px solid black";
+            // tempContainer.style.border = "1px solid black";
             tempContainer.style.height = "65px";
             tempContainer.style.width = "80%";
-            tempContainer.style.margin = "3px";
+            // tempContainer.style.margin = "3px";
             tempContainer.style.position = "relative";
             tempContainer.style.overflow = "hidden";
             dayContainer.appendChild(tempContainer);
@@ -140,7 +140,7 @@ function appendData(data) {
             // swiper.classList.add("swiper", `mySwiper${dayNumber}`);
             swiper.classList.add("swiper");
             swiper.id = `mySwiper${dayNumber}`
-            swiper.style.border = "1px solid black";
+            // swiper.style.border = "1px solid black";
             swiper.style.height = "50px";
             swiper.style.width = "85%";
             // tempContainer.style.margin = "3px";
@@ -180,10 +180,19 @@ function appendData(data) {
             buttonNext.id = `arrowRight${dayNumber}`;
             tempContainer.appendChild(buttonNext);
 
+            let svgArrowRight = document.createElement('span');
+            svgArrowRight.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#b2c8ff"><g><rect fill="none" height="24" width="24"/></g><g><path d="M22,12c0-5.52-4.48-10-10-10C6.48,2,2,6.48,2,12s4.48,10,10,10C17.52,22,22,17.52,22,12z M4,12c0-4.42,3.58-8,8-8 c4.42,0,8,3.58,8,8s-3.58,8-8,8C7.58,20,4,16.42,4,12z M16,12l-4,4l-1.41-1.41L12.17,13H8v-2h4.17l-1.59-1.59L12,8L16,12z"/></g></svg>'
+            buttonNext.appendChild(svgArrowRight);
+
+
             let buttonPrev = document.createElement('div');
             buttonPrev.classList.add("swiper-button-prev");
             buttonPrev.id = `arrowLeft${dayNumber}`;
             tempContainer.appendChild(buttonPrev);
+
+            let svgArrowLeft = document.createElement('span');
+            svgArrowLeft.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#b2c8ff"><g><rect fill="none" height="24" width="24"/></g><g><path d="M2,12c0,5.52,4.48,10,10,10c5.52,0,10-4.48,10-10S17.52,2,12,2C6.48,2,2,6.48,2,12z M20,12c0,4.42-3.58,8-8,8 c-4.42,0-8-3.58-8-8s3.58-8,8-8C16.42,4,20,7.58,20,12z M8,12l4-4l1.41,1.41L11.83,11H16v2h-4.17l1.59,1.59L12,16L8,12z"/></g></svg>';
+            buttonPrev.appendChild(svgArrowLeft);
 
         }
     }
